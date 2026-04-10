5c82f23adbcf2343798a6789d80cfdb75ca87483
iliashonia <iliashonia48@gmail.com>
2026-04-10 15:22:12 +0400

    Update guitars.js

--- guitars.js
+++ guitars.js
@@ -8,11 +8,74 @@ export const guitars = [
 
     },
     {
+        name:"Les Paul Customs",
+        year: 1954,
+        category: "Golden Era",
+        description:"The first Black Beauty. It was designed to look like a tuxedo. Unlike the Goldtop, this had an all-mahogany body and a unique Staple pickup in the neck." ,
+        image:"https://i.pinimg.com/736x/62/6f/8b/626f8b7abb5042c4126de1c529428842.jpg"
+    },
+    {
+        name: "Les Paul Junior",
+        year:1954,
+        category: "Golden Era",
+        description: "The budget model. It was a slab of mahogany with a single P-90 pickup and a wrap-around tailpiece. Very popular in punk rock later on.",
+        image: "https://i.pinimg.com/736x/a4/ce/78/a4ce7847bd1d0a7f78ea61e017ba755d.jpg"
+    },
+    {
+        name: "Les Paul Special",
+        year:1955,
+        category: "Golden Era",
+        description: "Similar to the Junior but with a TV Yellow finish and two P-90 pickups.",
+        image: "https://i.pinimg.com/736x/59/15/d5/5915d53153605be382f9c4c3d5e70685.jpg"
+    },
+    {
+        name: "Les Paul Standard Goldtop",
+        year: 1957,
+        category: "Golden Era",
+        description: "This was the first year they put the famous Humbucker pickups (PAF) into the Goldtop, replacing the P-90s.",
+        image: "https://i.pinimg.com/736x/cd/ec/47/cdec4724c745b18d01cab765ff22630f.jpg"
+    },
+    {
+        name:"Les Paul Standard(The First Burst)",
+        year: 1958,
+        category: "Golden Era",
+        description: "This is when they switched from Gold to the Sunburst finish, allowing the wood grain to show through for the first time.",
+        image: "https://i.pinimg.com/736x/77/b9/f9/77b9f9b5aebd3bf7116d52649aaa990a.jpg"
+    },
+    {
         name: "1959 Les Paul Standard",
         year: 1959,
         category: "Golden Era",
         description: "The Holy Grail with Sunburst finish and PAF humbuckers",
         image: "https://i.pinimg.com/736x/90/7c/18/907c18390c35260a7fbe3e510aefa0ed.jpg"
+    },
+    {
+        name:"Les Paul Standard",
+        year:1960,
+        category: "Golden Era",
+        description: "The last year of the original single-cutaway design. It's famous for having a Slim Taper neck, which was much thinner than the baseball bat necks of the 50s.",
+        image:"https://i.pinimg.com/736x/a7/1c/5a/a71c5ac654865172c6df3765d610de28.jpg" 
+    },
+    {   name:"Les Paul(SG Shape)",
+        year:1961,
+        category:"Norlin Era",
+        description:"For a few years, the Les Paul actually became the thin, double-cutaway shape we now call the SG.",
+        image:"https://i.pinimg.com/736x/ee/61/21/ee6121e8be82062f81972f284d9861b9.jpg"
+
+    },
+    {
+        name:"Les Paul Custom Reissue",
+        year:1968,
+        category:"Norlin Era",
+        description:"After being discontinued for years, Gibson brought the single-cutaway back. This version is famous for having a maple top on a mahogany body, unlike the '54 version.",
+        image:"https://i.pinimg.com/736x/39/f2/7c/39f27c6dec88e6394c9fba8d2108cee7.jpg"
+    },
+    {
+        name:"Les Paul Deluxe",
+        year:1969,
+        category:"Norlin Era",
+        description:"The hallmark of the Norlin Era. It features 'Mini-Humbuckers' and a 'pancake' body (layers of wood glued together).",
+        image:"https://i.pinimg.com/736x/b3/fd/7f/b3fd7f8abcc12709696422b9a6911c09.jpg"
     },
     {
         name: "Les Paul Deluxe",
@@ -22,9 +85,58 @@ export const guitars = [
         image: "https://i.pinimg.com/736x/ed/a1/51/eda151ff2a35be32de8852a219b67195.jpg"
     },
     {
+        name:"Les Paul Recording",
+        year:1971,
+        category:"Norlin Era",
+        description:"Les Paul's personal favorite. It had low-impedance pickups and a dozen switches for studio clean tones.",
+        image:"https://i.pinimg.com/736x/fe/21/b3/fe21b320d6d4c3f8f1312e6518339fc3.jpg"
+    },
+    {
+        name:"Les Paul Artisan",
+        year:1976,
+        category:"Norlin Era",
+        description:'A very fancy model from the 70s with "hearts and flowers" inlays on the fretboard and three humbuckers.',
+        image:"https://i.pinimg.com/736x/d3/c3/b9/d3c3b93dd5377e732658596eaeb9c0d3.jpg"
+    },
+    {
+        name:"Les Paul Studio",
+        year:1983,
+        category:"The Modern Standard",
+        descripion:'Designed for studio musicians who wanted the sound but didnt need the fancy "binding" (the white stripes around the edge) or flashy finishes.',
+        image:"https://i.pinimg.com/736x/90/da/0c/90da0cb71e90a5cd43a7e226edef63b8.jpg"
+    },
+    {
+        name:"Les Paul Classic",
+        year:1990,
+        category:"The Modern Standard",
+        descripion:'A tribute to the 1960 model. It was the first "reissue-style" guitar available to the general public with a slim neck and high-output ceramic pickups.',
+        image:"https://i.pinimg.com/736x/81/83/d6/8183d628e06593dc033840ed91b5c541.jpg"
+     },
+     {
+        name:"Les Paul Standard Plus",
+        year:1991,
+        category:"The Modern Standard",
+        decription:'Known for "Flame Maple" tops. This era started the trend of focusing on highly figured, "tiger-stripe" wood patterns.',
+        image:"https://i.pinimg.com/736x/26/e4/0e/26e40ed7de40afdcb6f280bef7e58493.jpg"
+     },
+     {
+        name:"Les Paul Standard",
+        year:2008,
+        category:"The Custom Shop & Innovations",
+        description:'A major tech update. It introduced a locking bridge, locking tuners, and an asymmetrical neck profile for comfort.',
+        image:"https://i.pinimg.com/736x/6a/18/13/6a181393a76e6e3295e28fa829d70c43.jpg"
+     },
+     {
+        name:"Les Paul 60s Standard (Unburst)",
+        year:2021,
+        category:"The Custom Shop & Innovations",
+        description:'Part of the current "Original Collection." It’s a return to the classic specs—no weight relief, no modern electronics, just heavy wood and vintage tone.',
+        image:"https://i.pinimg.com/736x/cf/e5/4e/cfe54e21721a94ad69b788f85dcf6175.jpg"
+     },
+    {
         name: "Les Paul Modern",
         year:2023,
-        category: "Modern",
+        category: "The Custom Shop & Innovations",
         description: "Lighter body, compound radius fingerboard, and coil-taps.",
         image:"https://i.pinimg.com/736x/d9/86/a8/d986a8077882dc5e2e31516dd36e4ba5.jpg"
     }
